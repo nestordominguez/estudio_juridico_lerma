a62f9a0a2048c628b9f39699628e6e34862b3a89
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2014-09-20 13:29:17 -0300

    home

---
+++ app/controllers/studio_controller.rb
@@ -0,0 +1,19 @@
+class StudioController < ApplicationController
+	def estudio		
+	end
+
+	def staff
+	end
+
+	def areas
+	end
+
+	def client
+	end
+
+	def publicaciones
+	end
+
+	def links
+	end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EstudioJuridicoLerma::Application.config.session_store :cookie_store, key: '_estudio_juridico_lerma_session'
