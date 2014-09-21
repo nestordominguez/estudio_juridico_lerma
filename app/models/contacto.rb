2ab3aff6f49c9c0718630fd0d479b6b0b1d5eed4
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2014-09-20 23:31:48 -0300

    validates model

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,2 +1,10 @@
 class Contacto < ActiveRecord::Base
+	validates :name, :email, :comentarios, presence: true
+	validates :name, length: { maximum: 20 }
+	validates :comentarios, length: { maximum: 160 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, format: { with: VALID_EMAIL_REGEX }
+  	validates :phone, :numericality => true, :length => 
+  				{ :minimum => 7, :maximum => 15 },
+  				:presence => {:message => 'No es un número de teléfono!'}
 end
